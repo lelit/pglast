3d489865869681acfcfde981d3670ce053e8a701
Lele Gaifax <lele@metapensiero.it>
2017-08-03 17:28:58 +0200

    First cut at an automatic enums extractor

---
+++ tools/extract_enums.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+# :Project:   pg_query -- Extract enums from PostgreSQL headers
+# :Created:   gio 03 ago 2017 14:54:39 CEST
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2017 Lele Gaifax
+#
+
+from os.path import basename
+import subprocess
+
+from pycparser import c_ast, c_parser
+
+
+HEADER = """\
+# -*- coding: utf-8 -*-
+# :Project:   pg_query -- Enums automatically extracted from %s @ %s
+#
+
+import enum
+
+"""
+
+
+def get_libpg_query_version():
+    result = subprocess.check_output(['git', 'describe', '--all', '--long'],
+                                     cwd='libpg_query')
+    return result.decode('utf-8').strip().split('/')[1]
+
+
+def preprocess(fname, cpp_args=[]):
+    result = subprocess.check_output(['cpp', '-E', *cpp_args, fname])
+
+    return result.decode('utf-8')
+
+
+def extract_enums(source):
+    typedefs = []
+    in_typedef = False
+    typedef = []
+
+    for line in source.splitlines():
+        if line and not line.startswith('#'):
+            if in_typedef:
+                typedef.append(line)
+                if line.startswith('}'):
+                    in_typedef = False
+                    typedefs.append(typedef)
+                    typedef = []
+            elif line.startswith('typedef enum '):
+                in_typedef = True
+                typedef.append(line)
+
+    parser = c_parser.CParser()
+    for typedef in typedefs:
+        yield parser.parse(''.join(typedef))
+
+
+def emit_constant(value):
+    return value.value
+
+
+def emit_binary_op(value):
+    assert isinstance(value.left, c_ast.Constant)
+    assert isinstance(value.right, c_ast.Constant)
+    return '%s %s %s' % (emit_constant(value.left),
+                         value.op,
+                         emit_constant(value.right))
+
+
+def int_enum_value_factory(index, enumerator):
+    if enumerator.value is None:
+        return '0' if index==0 else 'enum.auto()'
+
+    if isinstance(enumerator.value, c_ast.BinaryOp):
+        return emit_binary_op(enumerator.value)
+    elif isinstance(enumerator.value, c_ast.Constant):
+        return emit_constant(enumerator.value)
+    elif enumerator.value.name == 'PG_INT32_MAX':
+        return '0x7FFFFFFF'
+
+    assert enumerator.value.type == 'int'
+    return enumerator.value.value
+
+
+def char_enum_value_factory(index, enumerator):
+    assert enumerator.value.type == 'char'
+    return enumerator.value.value
+
+
+def determine_enum_type_and_value(enum):
+    type = 'enum.IntEnum'
+    value = int_enum_value_factory
+
+
+    for item in enum.values.enumerators:
+        if item.value:
+            if isinstance(item.value, c_ast.Constant) and item.value.type == 'char':
+                type = 'str, enum.Enum'
+                value = char_enum_value_factory
+                break
+
+    return type, value
+
+
+def write_enum(enum, output):
+    enum_type, value_factory = determine_enum_type_and_value(enum)
+    output.write('\n')
+    output.write('class %s(%s):\n' % (enum.name, enum_type))
+    for index, item in enumerate(enum.values.enumerators):
+        output.write('    %s = %s\n' % (item.name, value_factory(index, item)))
+
+
+def workhorse(args):
+    preprocessed = preprocess(args.header, ['-I%s' % idir for idir in args.include_directory])
+    with open(args.output, 'w', encoding='utf-8') as output:
+        output.write(HEADER % (basename(args.header), get_libpg_query_version()))
+        for node in sorted(extract_enums(preprocessed), key=lambda x: x.ext[0].name):
+            write_enum(node.ext[0].type.type, output)
+
+
+def main():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+    parser = ArgumentParser(description="PG enum extractor",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help="emit some noise in the process")
+    parser.add_argument('-n', '--dry-run', action='store_true', default=False,
+                        help="test run, just show what would happed")
+    parser.add_argument('-I', '--include-directory', action='append', metavar='DIR',
+                        help="add DIR to the list of include directories")
+    parser.add_argument('header',
+                        help="source header to be processed")
+    parser.add_argument('output',
+                        help="Python source to be created")
+
+    args = parser.parse_args()
+
+    workhorse(args)
+
+
+if __name__ == '__main__':
+    main()
