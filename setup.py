c8c0ce8eccc9d58124b8b7003dabefc60b114d1b
Lele Gaifax <lele@metapensiero.it>
2019-03-17 12:14:35 +0100

    Add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # :Created:   mer 02 ago 2017 15:20:43 CEST
 # :Author:    Lele Gaifax <lele@metapensiero.it>
 # :License:   GNU General Public License version 3 or later
-# :Copyright: © 2017, 2018 Lele Gaifax
+# :Copyright: © 2017, 2018, 2019 Lele Gaifax
 #
 
 from pathlib import Path
@@ -60,6 +60,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: SQL",
         # "Programming Language :: PL/SQL",
         "Topic :: Database",
