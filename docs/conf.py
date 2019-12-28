97af950b27a019a7a88bf943b47764f1583f7e93
Lele Gaifax <lele@metapensiero.it>
2019-12-28 09:34:23 +0100

    Comment out unused html_static_path setting

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pglast'
-copyright = '2017, 2018 Lele Gaifax'
+copyright = '2017, 2018, 2019 Lele Gaifax'
 author = 'Lele Gaifax'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -95,7 +95,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
