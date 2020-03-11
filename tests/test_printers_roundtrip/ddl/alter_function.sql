effcf9c197d27896953dbf142e726d571bfa8391
Ronan Dunklau <ronan@dunklau.fr>
2020-03-11 16:45:52 +0100

    Add support for Create / Alter / Drop PROCEDURE

--- tests/test_printers_roundtrip/ddl/alter_function.sql
+++ tests/test_printers_roundtrip/ddl/alter_function.sql
@@ -3,3 +3,5 @@ ALTER FUNCTION funca(somearg text) COST 100
 ALTER FUNCTION func_without_args() IMMUTABLE
 
 ALTER FUNCTION func() SET search_path TO public,schema2
+
+ALTER PROCEDURE func() SET schema TO public
