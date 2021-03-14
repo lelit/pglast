b4cb3ed9aa3305be6dd64f8df6d3acd0d4428183
Lele Gaifax <lele@metapensiero.it>
2021-03-14 21:13:24 +0100

    Remove test against CREATE EXTENSION ... FROM unpackaged, dropped in PG 13

--- tests/test_printers_prettification/ddl/create_extension.sql
+++ tests/test_printers_prettification/ddl/create_extension.sql
@@ -7,9 +7,3 @@ CREATE EXTENSION "Foobar" VERSION '1' CASCADE
 CREATE EXTENSION "Foobar"
   WITH version '1'
        CASCADE
-
-CREATE EXTENSION hstore SCHEMA public FROM unpackaged
-=
-CREATE EXTENSION hstore
-  WITH schema public
-       from 'unpackaged'
