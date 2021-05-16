e4ac638923a76572a4fd499a2622e97860c0a02e
Lele Gaifax <lele@metapensiero.it>
2021-05-16 11:11:34 +0200

    Remove duplicated tests

--- tests/test_printers_roundtrip/ddl/create_table.sql
+++ tests/test_printers_roundtrip/ddl/create_table.sql
@@ -213,16 +213,6 @@ CREATE TABLE measurement_y2016m07
     unitsales DEFAULT 0
 ) FOR VALUES FROM ('2016-07-01') TO ('2016-08-01')
 
-CREATE TABLE measurement_y2016m07
-    PARTITION OF measurement (
-    unitsales DEFAULT 0
-) FOR VALUES FROM ('2016-07-01') TO ('2016-08-01')
-
-CREATE TABLE measurement_y2016m07
-    PARTITION OF measurement (
-    unitsales DEFAULT 0
-) FOR VALUES FROM ('2016-07-01') TO ('2016-08-01')
-
 CREATE TABLE measurement_ym_older
     PARTITION OF measurement_year_month
     FOR VALUES FROM (MINVALUE, MINVALUE) TO (2016, MAXVALUE)
