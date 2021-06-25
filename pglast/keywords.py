4040a3d0985d08c0f57d0de7839d68c789592594
Lele Gaifax <lele@metapensiero.it>
2021-06-25 20:09:16 +0200

    Align printers doc and keywords.py to underlying libpg_query 13-2.0.5

--- pglast/keywords.py
+++ pglast/keywords.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# :Project:   pglast -- DO NOT EDIT: automatically extracted from kwlist.h @ 13.2
+# :Project:   pglast -- DO NOT EDIT: automatically extracted from kwlist.h @ 13.3
 # :Author:    Lele Gaifax <lele@metapensiero.it>
 # :License:   GNU General Public License version 3 or later
 # :Copyright: © 2017 Lele Gaifax
