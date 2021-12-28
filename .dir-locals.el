e2ed6bd1a89a9f51ddeebf30b6eee3e0eac19f0b
Lele Gaifax <lele@metapensiero.it>
2021-12-28 08:32:40 +0100

    Remote old coverage.el, replaced by https://github.com/lelit/pycov-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,1 @@
-((python-mode . ((eval . (let* ((root-dir (projectile-project-root))
-                                (cov.el (expand-file-name "coverage" root-dir))
-                                (cov.json (expand-file-name "coverage.json" root-dir)))
-                           (when (and (require 'pycov cov.el t)
-                                      (file-exists-p cov.json))
-                             (setq pycov-coverage-file cov.json)
-                             (pycov-mode)))))))
+((python-mode . ((pycov-coverage-file . "coverage.json"))))
