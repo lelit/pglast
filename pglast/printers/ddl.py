cfdddfe26f05f367778795df3083c8a17363cd95
Lele Gaifax <lele@metapensiero.it>
2024-11-26 07:39:14 +0100

    Remove spurious trailing space
    This fixes issue #165.

--- pglast/printers/ddl.py
+++ pglast/printers/ddl.py
@@ -1330,8 +1330,9 @@ class ConstrTypePrinter(IntEnumPrinter):
             output.write('ALWAYS ')
         elif node.generated_when == enums.ATTRIBUTE_IDENTITY_BY_DEFAULT:
             output.write('BY DEFAULT ')
-        output.write('AS IDENTITY ')
+        output.write('AS IDENTITY')
         if node.options:
+            output.space()
             with output.expression(True):
                 output.print_list(node.options, '')
 
@@ -1403,7 +1404,7 @@ def constraint(node, output):
             output.write(' USING INDEX TABLESPACE ')
             output.print_name(node.indexspace)
         if node.skip_validation:
-            output.write(' NOT VALID ')
+            output.write(' NOT VALID')
 
 
 @node_printer(ast.CreateAmStmt)
