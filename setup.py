e2b2e55e90b7d09009d896739244f5d61b048f23
Lele Gaifax <lele@metapensiero.it>
2025-11-12 07:56:40 +0100

    Move most packaging metadata to pyproject.toml
    This should fix #180.

--- setup.py
+++ setup.py
@@ -6,98 +6,46 @@
 # :Copyright: © 2017, 2018, 2019, 2020, 2021, 2022, 2024, 2025 Lele Gaifax
 #
 
+from os import fspath
 from pathlib import Path
 import subprocess
 import sys
 
-from setuptools import Extension, setup, find_packages
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 
 here = Path(__file__).absolute().parent
-with (here / 'README.rst').open(encoding='utf-8') as f:
-    README = f.read()
-with (here / 'CHANGES.rst').open(encoding='utf-8') as f:
-    CHANGES = f.read()
 
+libpg_query_dir = here / 'libpg_query'
+include_dir = libpg_query_dir / 'src' / 'postgres' / 'include'
+vendor_dir = libpg_query_dir / 'vendor'
 
-LIBPG_QUERY_DIR = here / 'libpg_query'
-INCLUDE_DIR = LIBPG_QUERY_DIR / 'src' / 'postgres' / 'include'
-VENDOR_DIR = LIBPG_QUERY_DIR / 'vendor'
+include_dirs = [libpg_query_dir, vendor_dir, include_dir]
+
+if sys.platform == 'win32':
+    include_dirs.append(include_dir / 'port' / 'win32')
+    include_dirs.append(include_dir / 'port' / 'win32_msvc')
+    make_cmd = make = ['nmake', '/F', 'Makefile.msvc', 'build']
+else:
+    make_cmd = ['make', '-s', 'build']
 
 
 class BuildLibPgQueryFirst(build_ext):
     def run(self):
-        if sys.platform == 'win32':
-            make = ['nmake', '/F', 'Makefile.msvc', 'build']
-        else:
-            make = ['make', '-s', 'build']
-        subprocess.check_call(make, cwd=LIBPG_QUERY_DIR)
+        subprocess.check_call(make_cmd, cwd=libpg_query_dir)
         super().run()
 
 
 setup(
-    name="pglast",
-    version="7.10",
-    url="https://github.com/lelit/pglast",
-
-    description="PostgreSQL Languages AST and statements prettifier",
-    long_description=README + '\n\n' + CHANGES,
-    long_description_content_type='text/x-rst',
-
-    author="Lele Gaifax",
-    author_email="lele@metapensiero.it",
-
-    license="GPL-3.0-or-later",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: SQL",
-        # "Programming Language :: PL/SQL",
-        "Topic :: Database",
-        "Topic :: Utilities",
-        ],
-    keywords="postgresql parser sql prettifier",
-
-    packages=find_packages('.'),
-
     cmdclass={'build_ext': BuildLibPgQueryFirst},
     ext_modules=[
-        Extension('pglast.parser',
-                  sources=['pglast/parser.c'],
-                  libraries=['pg_query'],
-                  include_dirs=list(
-                      map(str, (LIBPG_QUERY_DIR, VENDOR_DIR, INCLUDE_DIR)
-                          +
-                          ((INCLUDE_DIR / 'port' / 'win32',
-                            INCLUDE_DIR / 'port' / 'win32_msvc')
-                           if sys.platform == 'win32'
-                           else ()))),
-                  library_dirs=[str(LIBPG_QUERY_DIR)]),
+        Extension(
+            'pglast.parser',
+            sources=['pglast/parser.c'],
+            libraries=['pg_query'],
+            include_dirs=[fspath(p) for p in include_dirs],
+            library_dirs=[fspath(libpg_query_dir)]
+        ),
     ],
-
-    install_requires=[
-        'setuptools',
-    ],
-    extras_require={
-        'dev': [
-            'cython',
-            'metapensiero.tool.bump_version',
-            'pycparser',
-            'readme_renderer',
-        ]
-    },
-    entry_points="""\
-    [console_scripts]
-    pgpp = pglast.__main__:main
-    """,
 )
